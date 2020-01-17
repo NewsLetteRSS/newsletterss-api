8dfcceb9ae2b8cc306d6584c67081911283f8429
hwiveloper <hwiveloper@gmail.com>
2020-01-17 11:27:10 +0900

    2020 03/01 필터 인증,인가 미완성

---
+++ src/main/java/dev/newsletterss/api/NewsletterssApiApplication.java
@@ -0,0 +1,13 @@
+package dev.newsletterss.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsletterssApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsletterssApiApplication.class, args);
+	}
+
+}
