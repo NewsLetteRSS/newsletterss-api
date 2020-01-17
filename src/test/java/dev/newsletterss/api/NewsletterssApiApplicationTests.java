4d3166680ee6b65a9514f4e9c3f97ca24a7eafb5
hwiveloper <hwiveloper@gmail.com>
2020-01-17 11:27:10 +0900

    project init

---
+++ src/test/java/dev/newsletterss/api/NewsletterssApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.newsletterss.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsletterssApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
